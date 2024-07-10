b77fa85ba8ce39964f3713fcb14c614aca66cc22
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-10 16:00:46 +0300

    fix: change separator replacement location
    add separator replacement when defining IN_PATH variable

--- build/includer.js
+++ build/includer.js
@@ -2,11 +2,11 @@ const glob = require('glob');
 const path = require('node:path');
 const esbuild = require('esbuild');
 
-const IN_PATH = path.resolve('src', 'includer');
+const IN_PATH = path.resolve('src', 'includer').replace(/\\/g, '/');
 const OUT_PATH = 'includer';
 const FORMATS = ['cjs', 'esm'];
 
-const tsFiles = glob.sync(`${IN_PATH.replace(/\\/g, '/')}/**/*.ts`);
+const tsFiles = glob.sync(`${IN_PATH}/**/*.ts`);
 
 const build = (files, format) => Promise.all(files.map((name) => {
     const newFilePath = path.resolve(OUT_PATH, format, name.substring(IN_PATH.length + 1));

965909fd55bf401277f9ab877e4b44fbdcecc324
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-10 14:34:02 +0300

    feat: replace separators in paths for Windows users
    replace '\' to '/'

--- build/includer.js
+++ build/includer.js
@@ -6,7 +6,7 @@ const IN_PATH = path.resolve('src', 'includer');
 const OUT_PATH = 'includer';
 const FORMATS = ['cjs', 'esm'];
 
-const tsFiles = glob.sync(`${IN_PATH}/**/*.ts`);
+const tsFiles = glob.sync(`${IN_PATH.replace(/\\/g, '/')}/**/*.ts`);
 
 const build = (files, format) => Promise.all(files.map((name) => {
     const newFilePath = path.resolve(OUT_PATH, format, name.substring(IN_PATH.length + 1));

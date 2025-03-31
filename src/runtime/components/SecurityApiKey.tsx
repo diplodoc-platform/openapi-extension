0a39357c46dc55613c2b38717fc7e32242e8b939
zorox112 <zorox112@yandex-team.ru>
2025-03-31 11:57:57 +0300

    chron: class components -> function components

--- src/runtime/components/SecurityApiKey.tsx
+++ src/runtime/components/SecurityApiKey.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Col, Flex, Row, Text, TextArea} from '@gravity-ui/uikit';
 
 import {V3SecurityApiKey} from '../../includer/models';
@@ -10,85 +10,72 @@ type SecurityApiKeyProps = V3SecurityApiKey & {
     projectName: string;
 };
 
-export class SecurityApiKey extends React.Component<SecurityApiKeyProps, {value: string}> {
-    setValue: (value: string) => void;
+export function SecurityApiKey({in: inFromProps, name, projectName, close}: SecurityApiKeyProps) {
+    const [value, setValue] = useState<string>(getInitialValue(projectName));
 
-    constructor(props: SecurityApiKeyProps) {
-        super(props);
-
-        const {value} = getAuthByType(this.props.projectName, 'apiKey');
-        if (value) {
-            this.state = {
-                value:
-                    value.length > 6
-                        ? `${value.slice(0, 3)}${'*'.repeat(value.length - 6)}${value.slice(-3)}`
-                        : '*'.repeat(value.length),
-            };
-        } else {
-            this.state = {
-                value: '',
-            };
-        }
-
-        this.setValue = (value: string) => this.setState({value});
-    }
-
-    render() {
-        const {in: inFromProps, name, projectName} = this.props;
-        const {value} = this.state;
-        return (
+    return (
+        <Flex direction="column" width="100%" gap={4}>
             <Flex direction="column" width="100%" gap={4}>
-                <Flex direction="column" width="100%" gap={4}>
-                    <Row space={1}>
-                        <Col s={2}>
-                            <Text variant="subheader-1">In</Text>
-                        </Col>
-                        <Col s={10}>
-                            <Text variant="body-1">{MapperNames[inFromProps] ?? inFromProps}</Text>
-                        </Col>
-                    </Row>
+                <Row space={1}>
+                    <Col s={2}>
+                        <Text variant="subheader-1">In</Text>
+                    </Col>
+                    <Col s={10}>
+                        <Text variant="body-1">{MapperNames[inFromProps] ?? inFromProps}</Text>
+                    </Col>
+                </Row>
 
-                    <Row space={1}>
-                        <Col s={2}>
-                            <Text variant="subheader-1">Name</Text>
-                        </Col>
-                        <Col s={10}>
-                            <Text variant="body-1">{name}</Text>
-                        </Col>
-                    </Row>
+                <Row space={1}>
+                    <Col s={2}>
+                        <Text variant="subheader-1">Name</Text>
+                    </Col>
+                    <Col s={10}>
+                        <Text variant="body-1">{name}</Text>
+                    </Col>
+                </Row>
 
-                    <Row space={1}>
-                        <Col s={2}>
-                            <Text variant="subheader-1">Key</Text>
-                        </Col>
-                        <Col s={10}>
-                            <TextArea
-                                rows={2}
-                                value={value}
-                                onUpdate={this.setValue}
-                                onFocus={() => {
-                                    this.setValue('');
-                                }}
-                            />
-                        </Col>
-                    </Row>
-                </Flex>
-                <Flex justifyContent="end" spacing={{mb: 4}} gap={4}>
-                    <Button size="l" view="flat" onClick={this.props.close}>
-                        Cancel
-                    </Button>
-                    <Button
-                        size="l"
-                        view="action"
-                        onClick={() => {
-                            setAuth(projectName, {type: 'apiKey', value});
-                            this.props.close();
-                        }}
-                    >
-                        Save
-                    </Button>
-                </Flex>
+                <Row space={1}>
+                    <Col s={2}>
+                        <Text variant="subheader-1">Key</Text>
+                    </Col>
+                    <Col s={10}>
+                        <TextArea
+                            rows={2}
+                            value={value}
+                            onUpdate={setValue}
+                            onFocus={() => {
+                                setValue('');
+                            }}
+                        />
+                    </Col>
+                </Row>
             </Flex>
-        );
+            <Flex justifyContent="end" spacing={{mb: 4}} gap={4}>
+                <Button size="l" view="flat" onClick={close}>
+                    Cancel
+                </Button>
+                <Button
+                    size="l"
+                    view="action"
+                    onClick={() => {
+                        setAuth(projectName, {type: 'apiKey', value});
+                        close();
+                    }}
+                >
+                    Save
+                </Button>
+            </Flex>
+        </Flex>
+    );
+}
+
+function getInitialValue(projectName: string) {
+    const {value} = getAuthByType(projectName, 'apiKey');
+    if (value) {
+        return value.length > 6
+            ? `${value.slice(0, 3)}${'*'.repeat(value.length - 6)}${value.slice(-3)}`
+            : '*'.repeat(value.length);
+    } else {
+        return '';
     }
 }
--- src/runtime/components/SecurityOAuthImplicit.tsx
+++ src/runtime/components/SecurityOAuthImplicit.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Checkbox, Col, Flex, Row, Text, TextArea} from '@gravity-ui/uikit';
 
 import {V3SecurityOAuthImplicit} from '../../includer/models';
@@ -9,153 +9,133 @@ type SecurityOAuthImplicitProps = V3SecurityOAuthImplicit & {
     projectName: string;
 };
 
-export class SecurityOAuthImplicit extends React.Component<
-    SecurityOAuthImplicitProps,
-    {clientId: string; token: string; scopes: Record<string, boolean>; error?: string}
-> {
-    setClientId: (value: string) => void;
+export function SecurityOAuthImplicit({
+    flows: {
+        implicit: {scopes, authorizationUrl},
+    },
+    projectName,
+    close,
+}: SecurityOAuthImplicitProps) {
+    const {value} = getAuthByType(projectName, 'oauth2');
+    const [clientId, setClientId] = useState<string>('');
+    const [token, setToken] = useState<string>(value ?? '');
+    const [selectedScopes, setSelectedScopes] = useState<Record<string, boolean>>({});
+    const [error, setError] = useState<undefined | string>(undefined);
 
-    constructor(props: SecurityOAuthImplicitProps) {
-        super(props);
+    const hasToken = Boolean(token);
+    const scopesArray = Object.keys(scopes);
+    const hasScopes = Boolean(scopesArray.length);
+    const viewScopes = !hasToken && hasScopes;
 
-        const {value} = getAuthByType(this.props.projectName, 'oauth2');
-
-        this.state = {
-            clientId: '',
-            token: value ?? '',
-            scopes: {},
-            error: undefined,
-        };
-
-        this.setClientId = (clientId: string) => this.setState({clientId, error: undefined});
-    }
-
-    render() {
-        const {
-            flows: {
-                implicit: {scopes, authorizationUrl},
-            },
-        } = this.props;
-        const {clientId, error, token} = this.state;
-
-        const hasToken = Boolean(token);
-        const scopesArray = Object.keys(scopes);
-        const hasScopes = Boolean(scopesArray.length);
-        const viewScopes = !hasToken && hasScopes;
-
-        return (
-            <Flex direction="column" width="100%" gap={4}>
-                <Text variant="subheader-2">Authorization Url</Text>
-                <Text variant="body-1" color="secondary">
-                    {authorizationUrl}
-                </Text>
-
-                <Flex direction="column" width="100%" gap={4}>
-                    <Row space={1}>
-                        <Col s={2}>
-                            <Text variant="subheader-1">Flow</Text>
-                        </Col>
-                        <Col s={10}>
-                            <Text variant="body-1" color="secondary">
-                                implicit
-                            </Text>
-                        </Col>
-                    </Row>
-                    <Row space={1}>
-                        <Col s={2} style={{marginTop: 6}}>
-                            <Text variant="subheader-1">Client_id</Text>
-                        </Col>
-                        <Col s={10} style={{marginTop: hasToken ? 8 : 0}}>
-                            {hasToken ? (
-                                <Text variant="body-1">**********</Text>
-                            ) : (
-                                <TextArea
-                                    rows={1}
-                                    value={clientId}
-                                    onUpdate={this.setClientId}
-                                    onFocus={() => {
-                                        this.setClientId('');
-                                    }}
-                                    error={error}
-                                />
-                            )}
-                        </Col>
-                    </Row>
-                    <Row space={1}>
-                        <Col s={2}>{viewScopes && <Text variant="subheader-1">Scopes</Text>}</Col>
-                        <Col s={10} style={{marginTop: 4}}>
-                            {viewScopes && (
-                                <Flex direction="column" gap={1}>
-                                    {scopesArray.map((scope) => {
-                                        return (
-                                            <Checkbox
-                                                key={scope}
-                                                value={scope}
-                                                onUpdate={(value) =>
-                                                    this.setState((prevState) => ({
-                                                        ...prevState,
-                                                        scopes: {
-                                                            ...prevState.scopes,
-                                                            [scope]: value,
-                                                        },
-                                                    }))
-                                                }
-                                            >
-                                                {scope}
-                                            </Checkbox>
-                                        );
-                                    })}
-                                </Flex>
-                            )}
-                        </Col>
-                    </Row>
-                </Flex>
-
-                <Flex justifyContent="end" spacing={{mb: 4}} gap={4}>
-                    <Button size="l" view="flat" onClick={this.props.close}>
-                        Cancel
-                    </Button>
-                    {hasToken ? (
-                        <Button size="l" view="action" onClick={this.removeToken}>
-                            Log out
-                        </Button>
-                    ) : (
-                        <Button size="l" view="action" onClick={this.redirectToCreateToken}>
-                            Request
-                        </Button>
-                    )}
-                </Flex>
-            </Flex>
-        );
-    }
-
-    private redirectToCreateToken = () => {
-        if (!this.state.clientId) {
-            this.setState({error: 'Required field!'});
+    const redirectToCreateToken = () => {
+        if (!clientId) {
+            setError('Required field!');
             return;
         }
-        const url = new URL(this.props.flows.implicit.authorizationUrl);
+        const url = new URL(authorizationUrl);
         url.searchParams.set('response_type', 'token');
         url.searchParams.set('redirect_uri', window.location.href);
-        url.searchParams.set('client_id', this.state.clientId);
+        url.searchParams.set('client_id', clientId);
 
-        Object.entries(this.state.scopes).forEach(([scope, isActive]) => {
+        Object.entries(selectedScopes).forEach(([scope, isActive]) => {
             if (isActive) {
                 url.searchParams.append('scope', scope);
             }
         });
 
         window.location.href = url.toString();
-        this.props.close();
+        close();
     };
 
-    private removeToken = () => {
-        this.setState({
-            clientId: '',
-            error: undefined,
-            scopes: {},
-            token: '',
-        });
-        setAuth(this.props.projectName, {value: '', type: 'oauth2'});
+    const removeToken = () => {
+        setClientId('');
+        setError(undefined);
+        setSelectedScopes({});
+        setToken('');
+        setAuth(projectName, {value: '', type: 'oauth2'});
     };
+    return (
+        <Flex direction="column" width="100%" gap={4}>
+            <Text variant="subheader-2">Authorization Url</Text>
+            <Text variant="body-1" color="secondary">
+                {authorizationUrl}
+            </Text>
+
+            <Flex direction="column" width="100%" gap={4}>
+                <Row space={1}>
+                    <Col s={2}>
+                        <Text variant="subheader-1">Flow</Text>
+                    </Col>
+                    <Col s={10}>
+                        <Text variant="body-1" color="secondary">
+                            implicit
+                        </Text>
+                    </Col>
+                </Row>
+                <Row space={1}>
+                    <Col s={2} style={{marginTop: 6}}>
+                        <Text variant="subheader-1">Client_id</Text>
+                    </Col>
+                    <Col s={10} style={{marginTop: hasToken ? 8 : 0}}>
+                        {hasToken ? (
+                            <Text variant="body-1">**********</Text>
+                        ) : (
+                            <TextArea
+                                rows={1}
+                                value={clientId}
+                                onUpdate={(value) => {
+                                    setClientId(value);
+                                    setError(undefined);
+                                }}
+                                onFocus={() => {
+                                    setClientId('');
+                                }}
+                                error={error}
+                            />
+                        )}
+                    </Col>
+                </Row>
+                <Row space={1}>
+                    <Col s={2}>{viewScopes && <Text variant="subheader-1">Scopes</Text>}</Col>
+                    <Col s={10} style={{marginTop: 4}}>
+                        {viewScopes && (
+                            <Flex direction="column" gap={1}>
+                                {scopesArray.map((scope) => {
+                                    return (
+                                        <Checkbox
+                                            key={scope}
+                                            value={scope}
+                                            onUpdate={(value) =>
+                                                setSelectedScopes((prevState) => ({
+                                                    ...prevState,
+                                                    [scope]: value,
+                                                }))
+                                            }
+                                        >
+                                            {scope}
+                                        </Checkbox>
+                                    );
+                                })}
+                            </Flex>
+                        )}
+                    </Col>
+                </Row>
+            </Flex>
+
+            <Flex justifyContent="end" spacing={{mb: 4}} gap={4}>
+                <Button size="l" view="flat" onClick={close}>
+                    Cancel
+                </Button>
+                {hasToken ? (
+                    <Button size="l" view="action" onClick={removeToken}>
+                        Log out
+                    </Button>
+                ) : (
+                    <Button size="l" view="action" onClick={redirectToCreateToken}>
+                        Request
+                    </Button>
+                )}
+            </Flex>
+        </Flex>
+    );
 }

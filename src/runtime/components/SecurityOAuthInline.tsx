854568bfc42bb67a9456377762bbd9f936684604
zorox112 <zorox112@yandex-team.ru>
2025-03-31 11:57:57 +0300

    chron: class components -> function components

--- src/runtime/components/SecurityOAuthInline.tsx
+++ src/runtime/components/SecurityOAuthInline.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Col, Flex, Row, Text, TextArea} from '@gravity-ui/uikit';
 
 import {V3SecurityOAuthInline} from '../../includer/models';
@@ -9,85 +9,69 @@ type SecurityOAuthInlineProps = V3SecurityOAuthInline & {
     projectName: string;
 };
 
-export class SecurityOAuthInline extends React.Component<
-    SecurityOAuthInlineProps,
-    {value: string}
-> {
-    setValue: (value: string) => void;
-
-    constructor(props: SecurityOAuthInlineProps) {
-        super(props);
-
-        const {value} = getAuthByType(this.props.projectName, 'oauth2');
-        if (value) {
-            this.state = {
-                value:
-                    value.length > 6
-                        ? `${value.slice(0, 3)}${'*'.repeat(value.length - 6)}${value.slice(-3)}`
-                        : '*'.repeat(value.length),
-            };
-        } else {
-            this.state = {
-                value: '',
-            };
-        }
-
-        this.setValue = (value: string) => this.setState({value});
-    }
-
-    render() {
-        const {value} = this.state;
-        return (
+export const SecurityOAuthInline = ({close, projectName}: SecurityOAuthInlineProps) => {
+    const [value, setValue] = useState(getInitialValue(projectName));
+    return (
+        <Flex direction="column" width="100%" gap={4}>
             <Flex direction="column" width="100%" gap={4}>
-                <Flex direction="column" width="100%" gap={4}>
-                    <Row space={1}>
-                        <Col s={2}>
-                            <Text variant="subheader-1">In</Text>
-                        </Col>
-                        <Col s={10}>
-                            <Text variant="body-1">Header</Text>
-                        </Col>
-                    </Row>
-                    <Row space={1}>
-                        <Col s={2}>
-                            <Text variant="subheader-1">Name</Text>
-                        </Col>
-                        <Col s={10}>
-                            <Text variant="body-1">Authorization</Text>
-                        </Col>
-                    </Row>
-                    <Row space={1}>
-                        <Col s={2}>
-                            <Text variant="subheader-1">Value</Text>
-                        </Col>
-                        <Col s={10}>
-                            <TextArea
-                                rows={2}
-                                value={value}
-                                onUpdate={this.setValue}
-                                onFocus={() => {
-                                    this.setValue('');
-                                }}
-                            />
-                        </Col>
-                    </Row>
-                </Flex>
-                <Flex justifyContent="end" spacing={{mb: 4}} gap={4}>
-                    <Button size="l" view="flat" onClick={this.props.close}>
-                        Cancel
-                    </Button>
-                    <Button
-                        size="l"
-                        view="action"
-                        onClick={() => {
-                            setAuth(this.props.projectName, {type: 'oauth2', value});
-                            this.props.close();
-                        }}
-                    >
-                        Save
-                    </Button>
-                </Flex>
+                <Row space={1}>
+                    <Col s={2}>
+                        <Text variant="subheader-1">In</Text>
+                    </Col>
+                    <Col s={10}>
+                        <Text variant="body-1">Header</Text>
+                    </Col>
+                </Row>
+                <Row space={1}>
+                    <Col s={2}>
+                        <Text variant="subheader-1">Name</Text>
+                    </Col>
+                    <Col s={10}>
+                        <Text variant="body-1">Authorization</Text>
+                    </Col>
+                </Row>
+                <Row space={1}>
+                    <Col s={2}>
+                        <Text variant="subheader-1">Value</Text>
+                    </Col>
+                    <Col s={10}>
+                        <TextArea
+                            rows={2}
+                            value={value}
+                            onUpdate={setValue}
+                            onFocus={() => {
+                                setValue('');
+                            }}
+                        />
+                    </Col>
+                </Row>
             </Flex>
-        );
+            <Flex justifyContent="end" spacing={{mb: 4}} gap={4}>
+                <Button size="l" view="flat" onClick={close}>
+                    Cancel
+                </Button>
+                <Button
+                    size="l"
+                    view="action"
+                    onClick={() => {
+                        setAuth(projectName, {type: 'oauth2', value});
+                        close();
+                    }}
+                >
+                    Save
+                </Button>
+            </Flex>
+        </Flex>
+    );
+};
+
+function getInitialValue(projectName: string) {
+    const {value} = getAuthByType(projectName, 'oauth2');
+    if (value) {
+        return value.length > 6
+            ? `${value.slice(0, 3)}${'*'.repeat(value.length - 6)}${value.slice(-3)}`
+            : '*'.repeat(value.length);
+    } else {
+        return '';
     }
 }

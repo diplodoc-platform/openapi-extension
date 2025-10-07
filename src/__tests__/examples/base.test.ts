40cad5b5f569e6499eb18c20dcd99a55027c5d5e
3y3 <3y3@ya.ru>
2025-10-07 03:26:24 +0300

    chore: Add tests

--- src/__tests__/examples/base.test.ts
+++ src/__tests__/examples/base.test.ts
@@ -27,6 +27,37 @@ describe('openapi project with examples', () => {
         expect(page).toMatchSnapshot();
     });
 
+    it('renders example from ref', async () => {
+        const spec = new DocumentBuilder(name)
+            .response(200, {
+                description: 'Base 200 response',
+                schema: {
+                    type: 'object',
+                    properties: {
+                        title: DocumentBuilder.ref('Cat', 'Test description'),
+                    },
+                },
+            })
+            .component('Cat', {
+                type: 'object',
+                additionalProperties: {
+                    type: 'string',
+                },
+                example: {
+                    en_US: 'Nestle milk chocolate-caramel',
+                    ru_RU: 'Nestle молочный шоколад-карамель',
+                    es_ES: 'Nestlé chocolate con leche y caramelo',
+                },
+            })
+            .build();
+
+        const fs = await run(spec);
+
+        const page = fs.match(name);
+
+        expect(page).toMatchSnapshot();
+    });
+
     it('renders example from oneOf', async () => {
         const spec = new DocumentBuilder(name)
             .request({
